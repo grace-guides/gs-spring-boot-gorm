0660c8e0c53a75264dab9c0e6c758a10557a1278
Michael Yan <rain@rainboyan.com>
2024-03-15 06:10:29 +0800

    Generated Spring Boot app

---
+++ src/main/groovy/grace/guides/GraceApplication.groovy
@@ -0,0 +1,13 @@
+package grace.guides
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GraceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GraceApplication, args)
+	}
+
+}

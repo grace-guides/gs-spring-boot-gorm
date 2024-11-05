ae17b32d80086ea0b6e1c5405ee18136a21159e4
Michael Yan <rain@rainboyan.com>
2024-11-05 12:16:21 +0800

    Upgrade to Grace 2023.1.0-M3

--- app/services/grace/guides/PostService.groovy
+++ app/services/grace/guides/PostService.groovy
@@ -2,12 +2,11 @@ package grace.guides
 
 import grails.gorm.transactions.ReadOnly
 import grails.gorm.transactions.Transactional
+import grails.gorm.services.Service
 import groovy.transform.CompileStatic
-import org.springframework.stereotype.Service
 
 @CompileStatic
-@grails.gorm.services.Service(Post)
-@Service
+@Service(Post)
 interface PostService {
 
     List<Post> findAll()
